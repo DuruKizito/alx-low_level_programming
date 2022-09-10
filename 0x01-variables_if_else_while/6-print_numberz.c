3f4398e41d42b7a7cf91cfdb24c40ab2dae85cd5
root <durukizitoc@gmail.com>
2022-09-10 04:06:58 -0700

    variables

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -5,16 +5,17 @@
 /**
  * main - Entry point
  *
- * Return - Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	int d;
-	for (d = '0'; d <= '9'; d++)
-	{
-		putchar(d);
-	}
-	putchar('\n');
-	return (0);
+        int d;
+        for (d = '0'; d <= '9'; d++)
+
+        {
+                putchar(d);
+        }
+        putchar('\n');
+        return (0);
 }
