43b9085779154db9eab351754aad1abd7b6461eb
root <durukizitoc@gmail.com>
2022-09-10 03:24:30 -0700

    variables

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -4,7 +4,7 @@
 
 /**
  * main - Entry point
- * 
+ *
  * Return: Always 0 (Success)
  */
 
--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -11,9 +11,10 @@
 int main(void)
 {
 	int d;
-	for (d; '0'; d <= '9'); d++;
+	for (d = '0'; d <= '9'; d++)
+	{
 		putchar(d);
+	}
 	putchar('\n');
-
 	return (0);
 }
