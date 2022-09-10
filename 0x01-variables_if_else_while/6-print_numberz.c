9806d210cc9e58c716a98f99cc4b09c83113b641
root <durukizitoc@gmail.com>
2022-09-10 04:16:53 -0700

    variable

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -10,12 +10,12 @@
 
 int main(void)
 {
-        int d;
-        for (d = '0'; d <= '9'; d++)
-
-        {
-                putchar(d);
-        }
-        putchar('\n');
-        return (0);
+	int d;
+	for (d = '0'; d <= '9'; d++)
+		
+	{
+		putchar(d);
+	}
+	putchar('\n');
+	return (0);
 }
