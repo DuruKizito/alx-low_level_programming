853e63d693e4c9f3706b6e21eb014d3442b1e6de
root <durukizitoc@gmail.com>
2022-09-13 09:24:16 -0700

    Functions

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - prints the word _putchar
+ *
+ * Return: 0 on success execution.
+ */
+
+int main(void)
+{
+	char s[] = "_putchar";
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		_putchar(s[i]);
+		i++;
+	}
+	_putchar(10);
+
+	return (0);
+}
