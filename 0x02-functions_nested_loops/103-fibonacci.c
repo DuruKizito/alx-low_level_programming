7644d771dd9344750e99295a861eb89a8af37e82
root <durukizitoc@gmail.com>
2022-09-13 21:51:36 -0700

    Loop

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -5,11 +5,11 @@
  * Description: Print the sum of even Fibonacci numbers up to a fib value
  * not exceeding 4,000,000.
  * Return: 0
- * /
+ */
 
- int main(void)
- {
- 	int i = 1, j = 2, total = 0;
+int main(void)
+{
+	int i = 1, j = 2, total = 0;
 	int k;
 
 	while (j < 4000000)
@@ -18,7 +18,7 @@
 			total += j;
 
 		k = j;
-		j += 1;
+		j += i;
 		i = k;
 	}
 	printf("%d\n", total);
