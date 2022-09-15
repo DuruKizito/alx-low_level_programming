878d46fd95e7861006e384810690e6a743dc4690
root <durukizitoc@gmail.com>
2022-09-15 13:40:13 -0700

    main

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - test function that prints if integer is positive or negative
+ * main - tests function that prints if integer is positive or negative
  * Return: 0
  */
 
