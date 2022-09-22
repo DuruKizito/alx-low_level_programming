84812f8c6f130563e856b7cce29120d08e7249f2
root <durukizitoc@gmail.com>
2022-09-22 14:26:23 -0700

    string

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -3,7 +3,7 @@
 /**
  * _strncat - function to concatenate two strings and return a new string
  * with n elements of the appended string
- * 
+ *
  * @dest: string 1
  * @src: string 2
  * @n: number of chars to include from appended string
