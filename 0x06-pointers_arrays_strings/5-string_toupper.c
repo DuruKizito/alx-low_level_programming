afe95ca86b1c83d0e34082c8e4f0aa4363af1157
root <durukizitoc@gmail.com>
2022-09-23 14:17:46 -0700

    14

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -9,10 +9,10 @@ char *string_toupper(char *x)
 {
 	 int i = 0;
 
-	 while (x[i] != '\0')
+	 while (x[i])
 	 {
-		 if (x[i] > 96 && x[i] <= 123)
-			 x[i] -= 32;
+		 if (x[i] >= 97 && x[i] <= 122)
+			 x[i] = x[i] - 32;
 		 i++;
 	 }
 	 return (x);
