24793c08bb5383c169189f4aefd97e8fe14282e7
root <durukizitoc@gmail.com>
2022-09-22 22:02:00 -0700

    String

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -2,18 +2,18 @@
 
 /**
  * string_toupper - function to transform all lowercase in string to uppercase
- * @c: character string pointer
+ * @x: character string pointer
  * Return: char pointer
  */
-char *string_toupper(char *c)
+char *string_toupper(char *x)
 {
 	 int i = 0;
 
-	 while (c[i] != '\0')
+	 while (x[i] != '\0')
 	 {
-		 if (c[1] > 96 && c[i] < 123)
-			 c[i] -= 32;
+		 if (x[i] > 96 && x[i] <= 123)
+			 x[i] -= 32;
 		 i++;
 	 }
-	 return (c);
+	 return (x);
 }
