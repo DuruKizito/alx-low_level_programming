c6682ed63d1e983fe167d95cca01d419ec10da5e
Chukwuebuka Kizito Duru <durukizitoc@gmail.com>
2022-09-28 13:12:19 -0700

    Recursion

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+int check_prime(int, int);
+/**
+* is_prime_number -> prime identifier
+* @n: is number
+* Return: integer
+*/
+int is_prime_number(int n)
+{
+	return (check_prime(n, 1));
+}
+
+
+/**
+* check_prime -> it check prime numbers
+* @n: number
+* @i: iterator
+* Return: a 1 or 0
+*/
+int check_prime(int n, int i)
+{
+	if (n <= 1)
+		return (0);
+	if (n % i == 0 && i > 1)
+		return (0);
+	if ((n / i) < i)
+		return (1);
+	return (check_prime(n, i + 1));
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+Various task on Recursion
